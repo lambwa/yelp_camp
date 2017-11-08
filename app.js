4d46a4a394a2cc32aa6043814b23bd5a4f157e0a
Lameez Wakefield <lameez.wa@gmail.com>
2017-11-08 08:08:50 +0000

    changes made to app.js

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var campgroundRoutes    = require("./routes/campgrounds"),
 
 app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/yelp_camp_v13", {useMongoClient: true});
+// mongoose.connect("mongodb://localhost/yelp_camp_v13", {useMongoClient: true});
+mongoose.connect("mongodb://<lameez_wa>:<rusty>@ds151355.mlab.com:51355/yelpcamp", {useMongoClient: true});
+
 
 app.use(flash());
 //passport config
